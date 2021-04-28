9af893adea5e5d4a09e0d1d9613da23b06244f60
william-abboud <william_y_abboud@abv.bg>
2021-04-28 20:06:50 +0300

    Added webpack paths

---
+++ webpack.paths.js
@@ -0,0 +1,12 @@
+const { resolve, join } = require('path');
+
+const SRC = resolve(__dirname, 'src');
+const DIST = resolve(__dirname, 'dist');
+
+const IN_SRC = (subPath = '') => join(SRC, subPath);
+const IN_DIST = (subPath = '') => join(DIST, subPath);
+
+module.exports = {
+    IN_SRC,
+    IN_DIST,
+};
