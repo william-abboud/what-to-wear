f5e3c203b66966dbce668e57195f721d3e32d1b2
William <william_y_abboud@abv.bg>
2021-04-28 23:26:01 +0300

    Added dev and production configurations respecitvely

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,9 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { IN_SRC, IN_DIST } = require("./webpack.paths");
+const { IN_SRC, IN_DIST } = require('./webpack.paths');
 
 module.exports = {
-    mode: 'production',
     entry: IN_SRC('index.js'),
     output: {
         filename: 'main.js',
         path: IN_DIST(),
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: IN_SRC('index.html'),
-            minify: false,
-        })
-    ]
 };
