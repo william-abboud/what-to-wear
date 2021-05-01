5c0a8ff8c9f0b3df59618ee9accb98c82318ea93
William <william_y_abboud@abv.bg>
2021-05-01 14:56:14 +0300

    Refactored postcss config to use env variables and added more plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,47 @@
 const postcssPresetEnv = require('postcss-preset-env');
+const postcssNested = require('postcss-nested');
+const postcssVariables = require('postcss-css-variables');
+const postcssFlexboxFixes = require('postcss-flexbugs-fixes');
+const postcssImport = require('postcss-import');
+const cssnano = require('cssnano');
 
-module.exports = {
-  plugins: [
-    postcssPresetEnv({
-      stage: 1,
-      autoprefixer: {
-        grid: true,
-      },
-    }),
-  ],
+const env = process.env.NODE_ENV;
+
+/*
+  Order is important!
+  [1] Handle imports
+  [2] Handle variable declarations everywhere
+  [3] Handle nesting
+  [4] Handle Flexbox spec bugs
+  [5] Add Env preset
+*/
+const sharedPlugins = [
+  postcssImport,
+  postcssVariables,
+  postcssNested,
+  postcssFlexboxFixes,
+  postcssPresetEnv({
+    stage: 1,
+    autoprefixer: {
+      grid: true,
+    },
+  }),
+];
+
+const devPlugins = [
+  ...sharedPlugins,
+];
+
+const prodPlugins = [
+  ...sharedPlugins,
+  cssnano({
+    preset: 'default'
+  }),
+];
+
+const configs = {
+  development: { plugins: devPlugins },
+  production: { plugins: prodPlugins },
 };
+
+module.exports = configs[env];
