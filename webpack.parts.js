682fd89a1f668e727e48cc0bf3e9580301dbe35e
William <william_y_abboud@abv.bg>
2021-05-01 14:57:43 +0300

    Added sourcemaps and useful utility

--- webpack.parts.js
+++ webpack.parts.js
@@ -9,14 +9,26 @@ const devStyleConfig = {
       loader: 'css-loader',
       options: {
         importLoaders: 1,
+        sourceMap: true,
       },
     },
     {
-      loader: 'postcss-loader'
+      loader: 'postcss-loader',
+      options: {
+        sourceMap: true,
+      }
     }
   ],
 };
 
+const logToolConfig = (tool, configs) => {
+  const env = process.env.NODE_ENV;
+
+  console.log(`${tool} is running in ${env} mode with the following configuration:`);
+  console.log(JSON.stringify(configs[env], null, 4));
+};
+
 module.exports = {
   devStyleConfig,
+  logToolConfig,
 };
