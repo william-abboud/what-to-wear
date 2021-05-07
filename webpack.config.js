8120106467ee4c1b8b6c82579fc8172a9559cfee
William <william_y_abboud@abv.bg>
2021-05-07 11:12:56 +0300

    Refactored webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
-const { IN_SRC, IN_DIST } = require('./webpack.paths');
+const {
+  IN_SRC,
+  IN_DIST
+} = require('./webpack.paths');
+const {
+  styleConfig,
+  imagesConfig,
+  fontsConfig,
+  jsConfig
+} = require('./webpack.parts');
 
 module.exports = {
-    entry: IN_SRC('index.js'),
-    output: {
-        filename: 'main.js',
-        path: IN_DIST(),
-    },
+  entry: IN_SRC('index.js'),
+  output: {
+    filename: 'main.js',
+    path: IN_DIST(),
+    clean: true,
+  },
+  module: {
+    rules: [
+      imagesConfig,
+      fontsConfig,
+      styleConfig,
+      jsConfig,
+    ]
+  },
 };
