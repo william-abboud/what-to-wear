dd845a613ce6a84861ab513031398fc0e3b04ff4
William <william_y_abboud@abv.bg>
2021-05-07 11:16:19 +0300

    Added css extraction and minification and bundle file naming

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,13 +1,47 @@
+const ExtractCssChunks = require('extract-css-chunks-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { merge } = require('webpack-merge')
-
-const { IN_SRC } = require('./webpack.paths');
+const {
+  merge
+} = require('webpack-merge')
+const {
+  IN_SRC
+} = require('./webpack.paths');
 const commonConfig = require('./webpack.config');
 
 const prodConfig = {
   mode: 'production',
+  devtool: 'source-map',
+  output: {
+    filename: 'scripts/[name].[contenthash].js',
+  },
+  optimization: {
+    runtimeChunk: true,
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: [
+            'default',
+            {
+              discardComments: {
+                removeAll: true
+              },
+            },
+          ],
+        },
+      })
+    ]
+  },
   plugins: [
-    new HtmlWebpackPlugin({ template: IN_SRC('index.html') }),
+    new ExtractCssChunks({
+      filename: 'styles/[name].[contenthash].css',
+      chunkFilename: 'styles/[name].[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: IN_SRC('index.html'),
+      minify: false,
+    }),
   ],
 };
 
