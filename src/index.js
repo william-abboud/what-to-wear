9f24e8f09f1361011e10aca1919e0dddc2c1fa8c
William <william_y_abboud@abv.bg>
2021-05-07 11:07:05 +0300

    Imported images

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import './style.css';
+import './styles/style.css';
+import './scripts/components/message-card/message-card.css';
+import './assets/images/web.jpg';
+import './assets/images/web.webp';
+import './assets/images/web.avif';
+
 
 const sayHello = () => {
-    console.log('Hello World');
-    console.log('Something else here');
+  console.log('Hello World');
+  console.log('Something else here');
 };
 
 sayHello();
